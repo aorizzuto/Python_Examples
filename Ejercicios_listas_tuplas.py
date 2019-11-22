199ebba928d08d719570c5631d90e5e3620feb72
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-22 13:16:18 -0300

    Update Ejercicios_listas_tuplas.py

--- Ejercicios_listas_tuplas.py
+++ Ejercicios_listas_tuplas.py
@@ -112,3 +112,25 @@ print(abc)
 
 #['a', 'b', 'd', 'e', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 's', 't', 'v', 'w', 'y', 'z']
 
+
+# Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo
+
+palabra=(input("Escriba una palabra:")).upper()
+size=len(palabra)
+
+flag=1
+
+for i in range(size//2):
+    if palabra[i] != palabra[size-i-1]:
+        flag=0
+        break
+
+if flag:
+    print(f"{palabra} es un palindromo")
+else:
+    print(f"{palabra} no es un palindromo")
+
+"""
+Escriba una palabra: Sebastianaitsabes
+SEBASTIANAITSABES es un palindromo
+"""
