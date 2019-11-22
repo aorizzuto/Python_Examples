9c470340c199ad2b174bc9d0cc554228234f658e
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-22 14:20:46 -0300

    Update Ejercicios_listas_tuplas.py

--- Ejercicios_listas_tuplas.py
+++ Ejercicios_listas_tuplas.py
@@ -170,3 +170,28 @@ El mayor precio es: 33
 """
 
 
+
+
+# Escribir un programa que almacene dos vectores en dos listas y muestre por pantalla su producto escalar.
+
+vector1=[1,2,3,6]
+vector2=[-1,0,2,-3]
+
+def producto_escalar(v1,v2):
+    if len(v1) != len(v2):
+        return 0
+    
+    prod = 0
+    
+    for i in range(len(v1)):
+        prod=prod+v1[i]*v2[i]
+    
+    return prod
+
+print("El producto escalar de {} y {} es: {}".format(vector1,vector2,producto_escalar(vector1,vector2)))
+
+"""
+El producto escalar de [1, 2, 3, 6] y [-1, 0, 2, -3] es: -13
+"""
+
+
