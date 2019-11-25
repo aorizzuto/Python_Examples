1752dd355e2abd40e2315bd96bc86eb810b5aa73
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-25 08:15:58 -0300

    Update Ejercicios_021_030.py

--- Ejercicios_021_030.py
+++ Ejercicios_021_030.py
@@ -60,3 +60,20 @@ Ingrese numeros separados por coma:  3,1,5,2,4,7,6
 """
 
 
+# 4) Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn
+
+n=input("Ingrese numero. Se entregará el valor n+nn+nnn: ")
+
+nn=n+n
+nnn=n+nn
+
+
+print(int(nnn)+int(nn)+int(n))
+
+"""
+Ingrese numero. Se entregará el valor n+nn+nnn:  6
+738
+"""
+
+
+
