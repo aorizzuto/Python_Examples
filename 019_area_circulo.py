f189476172572cf602ac3af85828828db951be18
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-25 12:57:20 -0300

    Create 019_area_circulo.py

---
+++ 019_area_circulo.py
@@ -0,0 +1,11 @@
+# Write a Python program which accepts the radius of a circle from the user and compute the area
+
+radio = float(input("Ingrese radio: "))
+pi = 3.1415926
+
+print("El area del circulo es: ", pi*(radio**2))
+Ingrese radio:  3
+
+"""
+El area del circulo es:  28.2743334
+"""
