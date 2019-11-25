c3d35cc9a6683a9cfdcbb758161b278b4d954192
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-25 12:27:06 -0300

    Create 003_hola_alejandro.py

---
+++ 003_hola_alejandro.py
@@ -0,0 +1,9 @@
+# Escribir un programa que pregunte el nombre del usuario y después muestre por pantalla la cadena ¡Hola <nombre>!
+
+nombre=input("Introduzca el nombre:")
+print(f"¡Hola {nombre}!")
+
+"""
+Introduzca el nombre: Alejandro
+¡Hola Alejandro!
+"""
