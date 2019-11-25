46ca325e2b056aece9ccdc1d49a32e6ab1d9b920
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-25 12:34:15 -0300

    Create 011_mayor_de_edad.py

---
+++ 011_mayor_de_edad.py
@@ -0,0 +1,13 @@
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+edad=int(input("Introduzca su edad:"))
+
+if edad > 18:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
+
+"""
+Introduzca su edad: 33
+Es mayor de edad
+"""
