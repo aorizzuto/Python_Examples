8a9bf2a2a2d4d4d96824286b7e734d8128420e00
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-25 09:14:39 -0300

    Update Ejercicios_021_030.py

--- Ejercicios_021_030.py
+++ Ejercicios_021_030.py
@@ -131,3 +131,35 @@ Ingrese la fecha MENOR separada por espacios(Ej: DD MM YYYY): 03 03 1991
 Ingrese la fecha MAYOR separada por espacios(Ej: DD MM YYYY): 25 11 2019
 10494 dias de diferencia
 """
+
+
+
+
+
+# 7) Write a Python program to get a new string from a given string where "Is" has been added to the front. If the given string already begins with "Is" then return the string unchanged
+
+print("\n¿STRING EMPIEZA CON \"IS\"?")
+
+for i in range(2):
+    string=input("\nEscribir una oracion:")
+
+    str=string[0]+string[1]
+
+    if str.upper() == "IS":
+        print(string)
+    else:
+        string=string[:1].lower()+string[1:]
+        print("Is",string)
+
+"""
+¿STRING EMPIEZA CON "IS"?
+
+Escribir una oracion: Is there a house?
+Is there a house?
+
+Escribir una oracion: The penguin is called Alberto
+Is the penguin is called Alberto
+"""
+
+
+
