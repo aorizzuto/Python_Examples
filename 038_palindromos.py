61a153d977cc7b33428bbb3ee36f0794a1db3dc9
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-26 08:17:35 -0300

    Create 038_palindromos.py

---
+++ 038_palindromos.py
@@ -0,0 +1,21 @@
+# Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo
+
+palabra=(input("Escriba una palabra:")).upper()
+size=len(palabra)
+
+flag=1
+
+for i in range(size//2):
+    if palabra[i] != palabra[size-i-1]:
+        flag=0
+        break
+
+if flag:
+    print(f"{palabra} es un palindromo")
+else:
+    print(f"{palabra} no es un palindromo")
+
+"""
+Escriba una palabra: Sebastianaitsabes
+SEBASTIANAITSABES es un palindromo
+"""
