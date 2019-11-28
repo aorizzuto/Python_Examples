3c9145258aeb4938265f076a51109eb4f92e08a7
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-28 07:59:41 -0300

    Create 053_input_2_parametros.py

---
+++ 053_input_2_parametros.py
@@ -0,0 +1,15 @@
+# Write a Python program to input two integers in a single lineWrite a Python program to calculate the time runs (difference between start and current time) of a program
+
+print("Ingrese valores de X e Y separados por espacio")
+
+x, y = map(int, input().split())
+
+print("Los valores de X e Y son:\nX = {}\nY = {}".format(x,y))
+
+"""
+Ingrese valores de X e Y separados por espacio
+ 5 6
+Los valores de X e Y son:
+X = 5
+Y = 6
+"""
