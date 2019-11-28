4567f49464c08e609099720bebbb83caa3a0c300
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-28 07:28:31 -0300

    Create 050_limitar_caraceres.py

---
+++ 050_limitar_caraceres.py
@@ -0,0 +1,10 @@
+# Write a Python program to format a specified string to limit the number of characters to 6
+
+string = "Alejandro"
+print("El string original es:",string)
+print("El string limitado a 6 caracteres es:",string[:6])
+
+"""
+El string original es: Alejandr
+El string limitado a 6 caracteres es: Alejan
+"""
