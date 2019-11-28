c8be0c98cbd6f3858a407d1f4cf8f17b018c44eb
aorizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-11-27 21:13:21 -0300

    Update 046_ordenar_y_eliminar_duplicados_lista.py

--- 046_ordenar_y_eliminar_duplicados_lista.py
+++ 046_ordenar_y_eliminar_duplicados_lista.py
@@ -1,11 +1,11 @@
 # Ordenar y eliminar duplicados de una lista
 
 def ejercicio(lista):
-    lista_sin_repetidos = set(lista)
-    sorted(lista_sin_repetidos)
+    lista_sin_repetidos = list(set(lista))
+    lista_sin_repetidos.sort()
     return lista_sin_repetidos
 
-lista=[1,3,6,6,4,2,2,1,2,4,5]
+lista=[1, 3, 4, 5, 6, 100, 27]
 
 lista_final = ejercicio(lista)
 
