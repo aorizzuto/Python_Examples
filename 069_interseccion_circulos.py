e03fde6258e84be1313248d9cd1c46e969092c81
Alejandro Rizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-12-06 08:06:46 -0300

    Update 069_interseccion_circulos.py

--- 069_interseccion_circulos.py
+++ 069_interseccion_circulos.py
@@ -7,15 +7,14 @@ import numpy as np
 
    
 def grafico(lst):
-    p1=[float(lst[0]),float(lst[1])]   # x0,y0
-    p2=[float(lst[2]),float(lst[3])]   # x1,y1
-    p3=[float(lst[4]),float(lst[5])]   # x2,y2
-    p4=[float(lst[6]),float(lst[7])]   # x4,y4
+    p1=[float(lst[0]),float(lst[1]),float(lst[2])]   # x0,y0
+    p2=[float(lst[3]),float(lst[4]),float(lst[5])]   # x1,y1
 
-    x,y = calculo_x_y(p1,p2)
-    plt.plot(x, y, label='Linea 1')              # Dibujo la anterior linea
-    x,y = calculo_x_y(p3,p4)
-    plt.plot(x, y, label='Linea 2')              # Dibujo la anterior linea
+    x,y = calculo_x_y(p1)
+    plt.plot(x, y, label='Circulo 1')              # Dibujo la anterior linea
+    
+    x,y = calculo_x_y(p2)
+    plt.plot(x, y, label='Circulo 2')              # Dibujo la anterior linea
 
     ### Detalles del gráfico ###
     plt.title('Ejercicio Lineas Paralelas')  # Nombre del grafico
@@ -27,36 +26,27 @@ def grafico(lst):
     plt.ylim([0,10])         # Limites del eje Y
     plt.show()              # Habilito para mostrar el grafico
 
+
+
+
     
 
+
+
+def calculo_x_y(p):
+    th = np.linspace(0,2*np.pi,100)   # 100 Puntos entre x0 y x1
+    radio = p[2]
+ 
+    x = radio*np.cos(th)+p[0]
+    y = radio*np.sin(th)+p[1]
     
-### Calculo b de la ecuación de la recta ###
-
-def calc_b(p,x,y):
-    return (p[1] - (y/x)*p[0])
-
-
-
-
-
-### Calculo los valores de X y de Y entre dos puntos dados ###
-
-def calculo_x_y(p_1,p_2):
-    x=float(p_1[0])-float(p_2[0])   # Distancia entre puntos x0 y x1
-    y=float(p_1[1])-float(p_2[1])   # Distancia entre puntos y0 y y1
-
-    b = calc_b(p_1,x,y)
-    x2 = np.linspace(p_1[0],p_2[0],100)   # 100 Puntos entre x0 y x1
-    y2 = (y/x)*x2 + b                     # Ecuacion de la recta
+    return x,y
     
-    return x2,y2
-
-
 
 
 ### MAIN ###
     
-s = input("Inserte los 4 puntos de las 2 rectas separados por espacio (x1 y1 r1 x2 y2 r2):")
+s = input("Inserte los 3 puntos de las 2 circunferencias separados por espacio (x1 y1 r1 x2 y2 r2):")
 lst = list(map(int,s.split()))
 
 grafico(lst)
