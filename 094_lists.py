3a7c57334b157a1f16d762df991b5f08a3980f95
Alejandro Rizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-12-17 07:51:46 -0300

    Create 094_lists.py

---
+++ 094_lists.py
@@ -0,0 +1,44 @@
+# Multiplicar todos los elementos de una lista
+
+import numpy as np 
+
+x = [1,2,3,4,5]
+
+print(np.prod(x))
+
+"""
+120
+"""
+
+
+
+# Sumar todos los elementos de una lista
+
+x = [1,2,3,4,5]
+
+print(sum(x))
+
+"""
+15
+"""
+
+
+# Máx elemento de una lista
+
+x = [3,2,7,4,5]
+
+print(max(x))
+
+"""
+7
+"""
+
+# Mín elemento de una lista
+
+x = [3,2,7,4,5]
+
+print(min(x))
+
+"""
+2
+"""
