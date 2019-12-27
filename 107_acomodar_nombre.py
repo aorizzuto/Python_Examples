db50bd16dfc3781dffdeed12d7dad42dbf7e4061
Alejandro Rizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-12-27 13:51:45 -0300

    Create 107_acomodar_nombre.py

---
+++ 107_acomodar_nombre.py
@@ -0,0 +1,14 @@
+# Acomodar nombre aleJANdrO --> Alejandro
+
+def cap_me(i):
+    return (i.lower().capitalize())
+
+print(cap_me("aleJANdrO"))
+print(cap_me("sEBasTIaN"))
+print(cap_me("LuCIa"))
+
+"""
+Alejandro
+Sebastian
+Lucia
+"""
